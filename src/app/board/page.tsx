e899a813f14ef4c15ba545e9cf298cd89a1b0335
Tupalski Ilya <itupalski@gmail.com>
2024-02-17 17:35:28 +0400

    fix: add suspense (Sat Feb 17 17:35:28 +04 2024)

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,10 +1,13 @@
-import TableView from "@/components/board/Table";
+import TableView from "@/components/board/TableView";
+import { Suspense } from "react";
 
 export default function Board() {
     return (
         <main className="mt-28 mx-8">
             <h1 className="text-3xl mb-8">Board</h1>
-            <TableView />
+            <Suspense>
+                <TableView />
+            </Suspense>
         </main>
     );
 }
